34b4ed4c8f54fd98ae6b9d6e47c0ccb117ea0aef
Zachary Streeter <zacharylouis42@gmail.com>
2023-12-07 14:53:27 -0600

    nodejs installation

--- main_install.sh
+++ main_install.sh
@@ -56,6 +56,13 @@ pushd $HOME/.software/qutebrowser/
 EOL
 chmod +x "$QUTEBROWSER_SCRIPT"
 
+# Nodejs
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
+
 # Theme
 mkdir $HOME/.local/share/themes
 unzip ./icons_themes/Catppuccin-Macchiato-Standard-Lavender-Dark.zip && mv ./icons_themes/Catppuccin-Macchiato-Standard-Lavender-Dark $HOME/.local/share/themes
