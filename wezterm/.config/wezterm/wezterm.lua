023095f0b940597bfbf5402634bd6c13c53eb8cf
Zachary Streeter <zacharylouis42@gmail.com>
2024-02-10 12:09:50 -0600

    small changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -15,6 +15,7 @@ config.color_scheme = "Catppuccin Macchiato"
 config.font_size = 12
 config.font = wezterm.font("FiraCode Nerd Font")
 
+config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = "RESIZE"
 config.hide_mouse_cursor_when_typing = true
 config.hide_tab_bar_if_only_one_tab = true
