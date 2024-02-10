c78dbed15d1101154d9a05cc1a73c98742be654e
Zachary Streeter <zacharylouis42@gmail.com>
2024-02-10 09:27:52 -0600

    using yazi now

--- zsh/.config/zsh/commands.sh
+++ zsh/.config/zsh/commands.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-# Use lf to switch directories and bind it to ctrl-o
-lfcd () {
-    tmp="$(mktemp -uq)"
-    trap 'rm -f $tmp >/dev/null 2>&1 && trap - HUP INT QUIT TERM PWR EXIT' HUP INT QUIT TERM PWR EXIT
-    lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
-    fi
-}
-bindkey -s '^o' '^ulfcd\n'
+bindkey -s '^o' 'yazi\n'
