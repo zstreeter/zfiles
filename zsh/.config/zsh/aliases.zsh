fc927d483af2908ff401d8bba98682c17b0b2605
Zachary Streeter <zacharylouis42@gmail.com>
2025-04-26 12:49:16 -0500

    added QT_QPA_PLATFORM aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,4 +2,8 @@
 [ -x "$(command -v lvim)" ] && alias nvim="lvim" vimdiff="lvim -d"
 
 alias \
-	wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"'
+	wget='wget --hsts-file="$XDG_CACHE_HOME/wget-hsts"' \
+  freecad='QT_QPA_PLATFORM=xcb freecad' \
+  openscad='QT_QPA_PLATFORM=xcb openscad' \
+  ultimaker-cura='QT_QPA_PLATFORM=xcb UltiMaker-Cura-5.8.1-linux-X64.AppImage' \
+  rpi-imager='QT_QPA_PLATFORM=xcb rpi-imager'
